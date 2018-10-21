88cf8001b01975aa8f62d13015202c02cf4db3d0
Mustafa Cavdar <mustafacavdar07@gmail.com>
2018-10-21 19:57:51 +0300

    Done

--- src/tweetlikecounter/TweetLikeCounter.java
+++ src/tweetlikecounter/TweetLikeCounter.java
@@ -1,21 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tweetlikecounter;
 
-/**
- *
- * @author Mustafa
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+import twitter4j.Paging;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 public class TweetLikeCounter {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+    public static void main(String[] args)  {
+       
+        //Must be acquired from Twitter
+        String consumerKey = "";
+        String consumerSecret = "";
+        String accessToken = "";
+        String accessTokenSecret = "";
+        
+        //Account username whose likes will be counted
+        String screenName = "";
+        
+        HashMap<String, Integer> accLikes = new HashMap<String, Integer>();
+        List<Status> statuses = new ArrayList<Status>();
+                
+        try{
+            ConfigurationBuilder cb = new ConfigurationBuilder();
+            cb.setDebugEnabled(true)
+                    .setOAuthConsumerKey(consumerKey)
+                    .setOAuthConsumerSecret(consumerSecret)
+                    .setOAuthAccessToken(accessToken)
+                    .setOAuthAccessTokenSecret(accessTokenSecret);
+            
+            Paging paging = new Paging(1);
+            Twitter twitter = new TwitterFactory(cb.build()).getInstance(); 
+            
+            do{
+                statuses = twitter.getFavorites(screenName, paging);
+                for(Status s: statuses){
+                    String user = s.getUser().getScreenName();
+                    if(accLikes.containsKey(user))
+                        accLikes.put(user, accLikes.get(user) + 1);
+                    else
+                        accLikes.put(user, 1); 
+                    
+                }
+                paging.setPage(paging.getPage() + 1);
+                
+            }while(statuses.size() > 0);            
+            
+        }catch(TwitterException te){}              
+                
+        Set<Entry<String, Integer>> setOfEntries = accLikes.entrySet();
+        setOfEntries.forEach((entry) -> {
+            System.out.println(entry.getKey() + "\t" + entry.getValue());
+        });                
+    }    
 }
